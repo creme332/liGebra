f1c76d658b37aae9cbf3f74eaa953cfcd1fb2ef0
creme332 <65414576+creme332@users.noreply.github.com>
2023-01-25 11:14:57 +0400

    add gauss approx, det, minor functions

--- src/SquareMatrix.cpp
+++ src/SquareMatrix.cpp
@@ -1,8 +1,8 @@
 #include "SquareMatrix.h"
-#include <stdexcept>
 
 SquareMatrix::SquareMatrix(vector<vector<double>> initMatrix,
                            bool _isAugmented) {
+  calculations = "";
   if (isValid(initMatrix, _isAugmented)) {
     myMatrix = initMatrix;
     isAugmented = _isAugmented;
@@ -30,4 +30,119 @@ bool SquareMatrix::isValid(vector<vector<double>> initMatrix,
     }
   }
   return 1;
+}
+
+vector<vector<double>> SquareMatrix::getMinor(vector<vector<double>> A,
+                                              int row,
+                                              int col) {
+  vector<vector<double>> minor;
+  for (int i = 0; i < A.size(); i++) {
+    vector<double> b;
+    for (int j = 0; j < A.size(); j++) {
+      if (i != row && j != col) {
+        b.push_back(A[i][j]);
+      }
+    }
+    if (b.size() > 0)
+      minor.push_back(b);
+  }
+  return minor;
+}
+
+double SquareMatrix::getDeterminant(vector<vector<double>> A) {
+  if (A.size() == 1) {
+    return A[0][0];
+  }
+
+  int determinant = 0;
+  // loop through each element in row 0
+  for (int i = 0; i < A[0].size(); i++) {
+    // apply cofactor formula
+    double el = i % 2 == 0 ? A[0][i] : -A[0][i];
+
+    // get minor of current element
+    vector<vector<double>> minor = getMinor(A, 0, i);
+    determinant += getDeterminant(minor) * el;
+  }
+  return determinant;
+}
+
+double SquareMatrix::det() {
+  return getDeterminant(myMatrix);
+}
+
+bool SquareMatrix::is_diagonally_dominant() {
+  const int lastCol = isAugmented ? myMatrix[0].size() - 1 : myMatrix[0].size();
+  for (int row = 0; row < myMatrix.size(); row++) {
+    // get absolute sum of all elements on current row except diagonal element
+    double sum = 0;
+    for (int col = 0; col < lastCol; col++) {
+      if (row != col) {
+        sum += abs(myMatrix[row][col]);
+      }
+    }
+    if (abs(myMatrix[row][row]) < sum)
+      return 0;
+  }
+  return 1;
+}
+
+void SquareMatrix::solve_approx(const vector<double> initial_approx,
+                                const int iterations) {
+  if (!isAugmented) {
+    throw std::invalid_argument("Matrix must augmented.");
+  }
+  if (!is_diagonally_dominant()) {
+    throw std::invalid_argument("Matrix must be diagonally dominant");
+  }
+  if (iterations < 1) {
+    throw std::invalid_argument(
+        "Number of iterations must be a positive value");
+  }
+
+  const int vars = myMatrix.size();
+
+  if (initial_approx.size() != vars) {
+    throw std::invalid_argument(
+        "Size of approximation array must be equal to number of variables");
+  }
+  // ! CHECK if solutions exist
+  
+  // calculate approximations
+  vector<vector<double>> table(iterations + 1, initial_approx);
+  for (int row = 1; row < iterations + 1; row++) {
+    for (int col = 0; col < vars; col++) {
+      table[row][col] = myMatrix[col][vars];
+      for (int i = 0; i < vars; i++) {
+        if (i != col) {
+          table[row][col] -= myMatrix[col][i] * table[row - 1][i];
+        }
+      }
+      table[row][col] /= myMatrix[col][col];
+    }
+  }
+
+  // output table
+  const char separator = ' ';
+  const int nameWidth = 15;
+  const int numWidth = 10;
+
+  // output table header
+  cout << std::left << std::setw(nameWidth) << std::setfill(separator)
+       << "Iteration";
+  for (int col = 0; col < vars; col++) {
+    std::string a = "x" + std::to_string(col + 1);
+    cout << std::left << std::setw(nameWidth) << std::setfill(separator) << a;
+  }
+  cout << endl;
+
+  // output table values
+  for (int row = 0; row < iterations + 1; row++) {
+    cout << std::left << std::setw(nameWidth) << std::setfill(separator) << row;
+    for (int col = 0; col < vars; col++) {
+      cout << std::left << std::setw(nameWidth) << std::setfill(separator)
+           << std::fixed << std::setprecision(3) << table[row][col];
+    }
+    cout << endl;
+  }
 }
