8e336cce39769a9c4beb833158a02ccbf0ab022b
creme332 <65414576+creme332@users.noreply.github.com>
2023-01-27 11:32:56 +0400

    add tests for parameters of `solve_approx`

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -2,14 +2,14 @@
 #include "doctest.h"
 
 TEST_CASE("Test constructor") {
-  SUBCASE("2D matrix") {
+  SUBCASE("2D matrix as parameter") {
     CHECK_THROWS_AS(SquareMatrix A({{1, 1}}), std::exception);
     CHECK_THROWS_AS(SquareMatrix A({{1, 1}, {1}}), std::exception);
     CHECK_THROWS_AS(SquareMatrix A({}), std::exception);
     CHECK_NOTHROW(SquareMatrix A({{1, 2}, {3, 4}}));
     CHECK_NOTHROW(SquareMatrix A({{1}}));
   }
-  SUBCASE("Augmented matrix") {
+  SUBCASE("Augmented matrix as parameter") {
     CHECK_NOTHROW(SquareMatrix A({{1, 2, 3}, {3, 4, 5}}, 1));
     CHECK_NOTHROW(SquareMatrix A({{1, 2}, {3, 4}}, 1));
   }
@@ -46,20 +46,60 @@ TEST_CASE("Test determinant") {
   }
 }
 
-TEST_CASE("Test diagonal dominance") {
+TEST_CASE("Check for diagonal dominance") {
   SUBCASE("3x4 DD augmented matrix") {
     SquareMatrix A({{-22, 3, 8, 27}, {3, -5, 2, -14}, {3, 3, 21, 48}}, true);
     CHECK_EQ(A.is_diagonally_dominant(), true);
   }
 
-    SUBCASE("3x4 non-DD augmented matrix") {
+  SUBCASE("3x4 non-DD augmented matrix") {
     SquareMatrix A({{-2, 3, 8, 27}, {3, -5, 2, -14}, {3, 3, 21, 48}}, true);
     CHECK_EQ(A.is_diagonally_dominant(), 0);
   }
+
+  SUBCASE("3x3 non-DD matrix") {
+    SquareMatrix A({{-22, 3, 8}, {3, -5, 2}, {3, 3, 21}}, true);
+    CHECK_EQ(A.is_diagonally_dominant(), true);
+  }
 }
 
-//TEST_CASE("Test Gauss-Jacobi") {
-//  SquareMatrix A({{-2, 3, 8, 27}, {3, -5, 2, -14}, {3, 3, 21, 48}}, true);
-//  const vector<double> initial_approx = {0, 0, 0};
-//  A.solve_approx(initial_approx, 4);
-//}
+TEST_CASE("Test Gauss-Jacobi and Gauss-Seidel") {
+  SUBCASE("Test input parameters") {
+    SquareMatrix A({{4, -1, -1, 3}, {-2, 6, 1, 9}, {-1, 1, 7, -6}}, true);
+    SUBCASE("Invalid initial_approx") {
+      CHECK_THROWS_WITH(
+          A.solve_approx(1, {0, 0, 0, 0}),
+          "Size of approximation array must be equal to number of variables");
+    }
+
+    SUBCASE("Invalid number of iterations places") {
+      CHECK_THROWS_WITH(A.solve_approx(1, {0, 0, 0}, -1),
+                        "Number of iterations must be a positive value");
+    }
+
+    SUBCASE("Invalid decimal places") {
+      CHECK_THROWS_WITH(A.solve_approx(1, {0, 0, 0}, 5, 0),
+                        "Number of decimal places must be a positive value");
+    }
+
+    SUBCASE("Matrix is not augmented") {
+      SquareMatrix A({{4, -1, -1}, {-2, 6, 1}, {-1, 1, 7}}, false);
+      CHECK_THROWS_WITH(A.solve_approx(1, {0, 0, 0}), "Matrix must augmented.");
+    }
+
+    SUBCASE("Matrix has 0s in its leading diagonal") {
+      SquareMatrix A({{0, -1, -1, 3}, {-2, 6, 1, 9}, {-1, 1, 7, -6}}, true);
+      CHECK_THROWS_WITH(
+          A.solve_approx(1, {0, 0, 0}),
+          "Leading diagonal elements of matrix must be non-zero.");
+    }
+  }
+
+  SUBCASE("Test values obtained") {
+    SquareMatrix A({{6, -1, -5, -19}, {-1, -7, -1, -22}, {-2, 3, 8, 27}}, true);
+    A.solve_approx(1, {1, 100, 50}, 100);
+  }
+
+  // SquareMatrix A({{-7, -1, 2, 3}, {1, 9, -3, 2}, {3, -2, 5, -1}}, true);
+  //  SquareMatrix A({{5, 2, 21}, {1, 2, 8}}, true);
+}
